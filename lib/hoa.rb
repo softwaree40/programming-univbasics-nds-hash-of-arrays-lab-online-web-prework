3f89cb47f64ebe38ffa5edfd7633f498e7835f1e
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 22:24:10 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,4 +1,4 @@
-lBASE_HOA = {
+BASE_HOA = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
@@ -9,7 +9,7 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
    
-  lBASE_HOA[show] << name
-  return lBASE_HOA
+  BASE_HOA[show] << name
+  return BASE_HOA
  
 end
