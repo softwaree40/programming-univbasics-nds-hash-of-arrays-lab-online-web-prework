87b81d3dc1f131be264b3fc06ca3ee87a4cb736b
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 22:18:21 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -9,7 +9,7 @@ def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
    
-  IBASE_HOA[show] << name
-  return IBASE_HOA
+  1BASE_HOA[show] << name
+  return 1BASE_HOA
  
 end
