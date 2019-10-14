53dd293ff4a195070fd45f9127c7dd3c3d0f5bc6
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-14 21:33:02 +0000

    Done.

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,4 +1,4 @@
-BASE_HOA = {
+lBASE_HOA = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
@@ -8,4 +8,11 @@ BASE_HOA = {
 def add_character(show, name)
   # Write your implementation here
   # Should return the array of the 'show' argument
+   lBASE_HOA[show] << name
+   binding.pry
+  return  lBASE_HOA
+  
+  
+  
+  
 end
